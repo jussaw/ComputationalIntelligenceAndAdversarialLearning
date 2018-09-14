9534780a7dbf25364cd9da70b0793d7605cf6796
Justin Sawyer <jtsaw36@gmail.com>
2018-09-14 16:19:58 -0500

    Adds writing samples

--- Homework 1/FeatureExtractor.py
+++ Homework 1/FeatureExtractor.py
@@ -1,3 +1,38 @@
+import math
+ourFileName = "../Articles/1000_1.txt"
+
+def createFeatureVector(fileName=None):
+    file = open(fileName, "r")
+    fileBody = file.read()#.lower()
+    fileFeatureVector = [0] * 128
+
+    for letter in fileBody:
+        fileFeatureVector[ord(letter)] = fileFeatureVector[ord(letter)] + 1
+        print letter
+
+    i = 0
+    for num in fileFeatureVector:
+        print(chr(i) + " - " + str(num))
+        #print(str(i) + " - " + str(num))
+        i = i + 1
+    #print fileBody
+    return fileFeatureVector
 
 
-print "Hello World"
+def normalize(numVectorIn=None):
+    sum = 0
+    numVector = numVectorIn
+    for num in numVector:
+        sum = sum + (num**2)
+    magnitude = math.sqrt(sum)
+    i = 0
+    for num in numVector:
+        numVector[i] = num / magnitude
+        i = i + 1
+
+    return numVector
+
+
+
+
+createFeatureVector(ourFileName)
