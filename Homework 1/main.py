cc13250051bac29f2f9a210ded378c5e76dfb128
Justin Sawyer <jtsaw36@gmail.com>
2018-09-15 15:02:19 -0500

    Some changes

---
+++ Homework 1/main.py
@@ -0,0 +1,14 @@
+import FeatureExtractor
+
+
+
+ourFileName = "../Articles/1000_1.txt"
+featureVector = FeatureExtractor.createFeatureVector(ourFileName)
+normalizedVector = FeatureExtractor.normalize(featureVector)
+
+i = 32
+for num in normalizedVector:
+    print (str(i) + " - " + str(num))
+    i += 1
+
+#frequencyVector = termFrequency(featureVector)
