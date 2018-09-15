84b72a22deac835fb1fcf1ec690231394957f988
Elijah Hampton <elijahhampton880@gmail.com>
2018-09-15 16:03:14 -0500

    Adds write file feature

--- Homework 1/FileUtil.py
+++ Homework 1/FileUtil.py
@@ -1,4 +1,12 @@
+import os.path
 
+def writeFile(numVector=None, fileName=None):
+    writePath = "../Feature Vectors/"
 
-def writeFile(numVector=None):
-    
+    completeName = os.path.join(writePath, fileName+".txt")
+    with open(completeName, "w") as file:
+        file.write(fileName)
+        file.write(",")
+        for num in numVector:
+            file.write(str(num))
+            file.write(",")
--- Homework 1/main.py
+++ Homework 1/main.py
@@ -1,4 +1,5 @@
 import FeatureExtractor
+import FileUtil
 
 ourFileName = "../Articles/1000_1.txt"
 featureVector = FeatureExtractor.createFeatureVector(ourFileName)
@@ -8,5 +9,6 @@ i = 32
 for num in normalizedVector:
     print (str(i) + " - " + str(num))
     i += 1
+    FileUtil.writeFile(normalizedVector, "test")
 
 #frequencyVector = termFrequency(featureVector)
