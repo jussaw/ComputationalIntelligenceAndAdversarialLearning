bcb3541e1368b2bd1bedb1da21f9a68d9eb7c9e5
Justin Sawyer <jtsaw36@gmail.com>
2018-09-16 11:56:42 -0500

    Adds comments for creating feature vectors and writing them to file

--- Homework 1/main.py
+++ Homework 1/main.py
@@ -11,10 +11,16 @@ fileNames = FileUtil.getFileNames()
 if os.path.isfile("../Feature Vectors/output.txt"):
     os.remove("../Feature Vectors/output.txt")
 
+# Create a feature vector and normalized feature vector for each
+# file then write both vectors to their corresponding files.
 for file in fileNames:
+    # fileArg is set to directory of each files
+    # fVector is feature vector of each file then it is written to file
     fileArg = "../Articles/" + file
     fVector = FeatureExtractor.createFeatureVector(fileArg)
     FileUtil.writeFile(fVector, "output", file)
 
+    # fNormalizedVector is noramlized version of fileFeatureVector
+    # Then we write it to the normalized feature vector file
     fNormalizedVector = FeatureExtractor.normalize(fVector)
     FileUtil.writeFile(fNormalizedVector, "outputNormalized", file)
