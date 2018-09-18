184ecea35e73b3f1afe79f975a6c7ce2e74f0bdf
Justin Sawyer <jtsaw36@gmail.com>
2018-09-18 14:47:44 -0500

    Adds comments to the functions

--- Homework 1/CalculateStatistics.py
+++ Homework 1/CalculateStatistics.py
@@ -1,5 +1,8 @@
 
-
+# Takes in an input of a feature vector
+# Determines the number of senteces by calculating the total number
+# of exclamation points, periods, and question marks.
+# Returns the total number calculated
 def calculateAmountOfSentences(featureVector=None):
     total = 0
     total += featureVector[33-32]
@@ -11,7 +14,10 @@ def calculateAmountOfSentences(featureVector=None):
 
     return total
 
-
+# Takes in an input of a feature vector.
+# Determines the total number of characters by adding up all the values
+# in the feature vector.
+# Return the calculated total.
 def calculateAmountOfChars(featureVector=None):
     total = 0
     for n in featureVector:
@@ -19,11 +25,20 @@ def calculateAmountOfChars(featureVector=None):
 
     return total
 
-
+# Takes in an input of feature vector.
+# Determines the total number of words by calculating the number of spaces
+# then adding 1.
+# Returns that calculated total
 def calculateAmountOfWords(featureVector=None):
     total = featureVector[32-32] + 1
+
     return total
 
-
+# Takes in an input of total and number of feature vectors.
+# Determines the average by dividing the the total by the number
+# of feature vectors.
+# Returns that average
 def calculateAverage(total, numOfFeatureVectors):
-    return total / numOfFeatureVectors
+    average = total / numOfFeatureVectors
+
+    return average
