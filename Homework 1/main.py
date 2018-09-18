0412159c54d3b4daf5482d4a1a3a7fdc31531ad1
Justin Sawyer <jtsaw36@gmail.com>
2018-09-18 14:45:38 -0500

    Calculates and Prints averages with functions from CalculateStatistics

--- Homework 1/main.py
+++ Homework 1/main.py
@@ -1,13 +1,15 @@
 import os
 import FeatureExtractor
 import FileUtil
+import CalculateStatistics
 
-#Run web Crawler
+# Run web Crawler
 
-#Find article names in folder
+# Find article names in folder
 fileNames = FileUtil.getFileNames()
+fileNames.sort()
 
-#Delete output files
+# Delete output files
 if os.path.isfile("../Feature Vectors/output.txt"):
     os.remove("../Feature Vectors/output.txt")
 if os.path.isfile("../Feature Vectors/outputNormalized.txt"):
@@ -15,6 +17,12 @@ if os.path.isfile("../Feature Vectors/outputNormalized.txt"):
 
 # Create a feature vector and normalized feature vector for each
 # file then write both vectors to their corresponding files.
+# We also calculate the average number of chars, words, and sentences
+# in the feature vectors.
+numOfFeatureVectors = len(fileNames)
+totalChars = 0
+totalWords = 0
+totalSentences = 0
 for file in fileNames:
     # fileArg is set to directory of each files
     # fVector is feature vector of each file then it is written to file
@@ -22,7 +30,24 @@ for file in fileNames:
     fVector = FeatureExtractor.createFeatureVector(fileArg)
     FileUtil.writeFile(fVector, "output", file)
 
+    # Calculates the total number of chars, words, and sentences on
+    # every iteration
+    totalChars += CalculateStatistics.calculateAmountOfChars(fVector)
+    totalWords += CalculateStatistics.calculateAmountOfWords(fVector)
+    totalSentences += CalculateStatistics.calculateAmountOfSentences(fVector)
+
     # fNormalizedVector is noramlized version of fileFeatureVector
     # Then we write it to the normalized feature vector file
     fNormalizedVector = FeatureExtractor.normalize(fVector)
     FileUtil.writeFile(fNormalizedVector, "outputNormalized", file)
+
+
+# Calculates the average number of chars, words, and sentences.
+averageChars = CalculateStatistics.calculateAverage(totalChars, numOfFeatureVectors)
+averageWords = CalculateStatistics.calculateAverage(totalWords, numOfFeatureVectors)
+averageSentences = CalculateStatistics.calculateAverage(totalSentences, numOfFeatureVectors)
+
+# Prints the average number of chars, words, and sentences.
+print("Average amount of chararacters = " + str(averageChars))
+print("Average amount of words = " + str(averageWords))
+print("Average amount of sentences = " + str(averageSentences))
