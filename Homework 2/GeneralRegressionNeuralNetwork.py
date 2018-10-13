7a6e70f58de39aed956d5ea5b327fd87bcc4708c
elijahhampton <elijahhampton880@gmail.com>
2018-10-13 08:52:07 -0500

    Homework 2 Base code

---
+++ Homework 2/GeneralRegressionNeuralNetwork.py
@@ -0,0 +1,30 @@
+hfs = []
+t = []
+
+def compute_outputs(t_q, t, sigma):
+  for i in range(0, len(t)):
+    hfs.append(hf(tq_0,t[i],sigma))   # Compute  the kernels
+
+  for i in range(0,len(t)):
+    for j in range(0,len(dq_0)):
+        dq_0[i] += hfs[i]*d[i][j] #First summation Unit
+
+  sum_hfs = 0
+
+    for i in range(0,len(t)):
+        sum_hfs += hfs[i]
+        for j in range(0,3):
+            dq_0[j] = dq_0[j]/sum_hfs #Output vector in decicmal form
+
+
+# Euclidean Distance Squared
+def dist_sqrd(t_q,t_i):
+    sum = 0.0
+    for i in range(0,len(t_q)):
+        sum += math.pow((t_q[i] - t_i[i]),2.0)
+    return sum
+
+
+# Gaussian Kernel
+def hf(t_q,t_i,sigma):
+    return math.exp(-dist_sqrd(t_q,t_i)/pow((2.0*sigma),2.0))
